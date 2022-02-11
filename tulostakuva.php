53cf30d8f50c448674c8d5a5348b4ad2bbc99d47
ropsupops <99011693+ropsupops@users.noreply.github.com>
2022-02-11 12:07:26 +0200

    Taulukkomuuttuja lisätty

--- tulostakuva.php
+++ tulostakuva.php
@@ -7,20 +7,41 @@
     <title>Kuvan tulostus</title>
 </head>
 <body>
+    <?php
+        $kuvat = array(
+                    array(  "src"=>"./meme1", 
+                            "alt"=>"Mies ja perhonen",
+                            "height"=>"600px",
+                            "widht"=>"600px"),
+                    array(  "src"=>"./meme2", 
+                            "alt"=>"Yllättynyt Pikachu",
+                            "height"=>"600px",
+                            "widht"=>"600px"),
+                    array(  "src"=>"./meme3", 
+                            "alt"=>"Mahtava Shiba Inu",
+                            "height"=>"600px",
+                            "widht"=>"600px"),
+                    array(  "src"=>"./meme4", 
+                            "alt"=>"Pastakauhanaamio",
+                            "height"=>"600px",
+                            "widht"=>"600px")
+                    );
+    ?>
     <h1>Päivitä sivu, jotta saat satunnaisen kuvan</h1>
     <img 
-    src="<?php 
-        echo "666";
-    ?>" 
-    alt="<?php 
-        echo "666";
-    ?>" 
-    height="<?php 
-        echo "666";
-    ?>" 
-    width="<?php 
-        echo "666";
-    ?>">
+        src="<?php 
+            echo "666";
+        ?>" 
+        alt="<?php 
+            echo "666";
+        ?>" 
+        height="<?php 
+            echo "666";
+        ?>" 
+        width="<?php 
+            echo "666";
+        ?>"
+    >
     <p><?php ?></p>
 </body>
 </html>
