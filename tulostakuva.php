35c9bce7f17c33269d624928c26ae5c6c12456b1
ropsupops <99011693+ropsupops@users.noreply.github.com>
2022-02-11 10:43:11 +0200

    Tässä pohja koodille

---
+++ tulostakuva.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kuvan tulostus</title>
+</head>
+<body>
+    <h1>Tämä sivu tulostaa satunnaisen kuvan, kun päivität sivun</h1>
+    <img 
+    src="<?php 
+
+    ?>" 
+    alt="<?php 
+
+    ?>" 
+    height="<?php 
+
+    ?>" 
+    width="<?php 
+    
+    ?>">
+    <p><?php ?></p>
+</body>
+</html>
